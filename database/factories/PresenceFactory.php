2d8933677f0feec3ba9e8adbb75969b272f820ff
LaVigne <lavigne10@gmail.com>
2021-08-28 20:11:57 +0000

    Repush PresenceFactory

---
+++ database/factories/PresenceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Presence;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PresenceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Presence::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'extended_user_id'=>1,
+            "isEnter"=>$this->faker->boolean()
+        ];
+    }
+}
